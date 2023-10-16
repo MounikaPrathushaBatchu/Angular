0256dcf3a2170253e98942bd9757acd3ff29c56a
Mounika Batchu <mounikabatchu5@gmail.com>
2023-10-16 12:48:32 +0530

    Name changed DempApp to DemoApp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DempApp'`, () => {
+  it(`should have as title 'DemoApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DempApp');
+    expect(app.title).toEqual('DemoApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DempApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DemoApp app is running!');
   });
 });
